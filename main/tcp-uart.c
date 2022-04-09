8c661c3976068b252c07896b450427f098419ff3
vaca <wsmslgh@gmail.com>
2022-04-09 07:23:03 -0700

    dsafawsedgfchv

--- main/tcp-uart.c
+++ main/tcp-uart.c
@@ -7,10 +7,29 @@
 #include "esp_event.h"
 #include "freertos/semphr.h"
 #include <sys/socket.h>
-
+#include "driver/uart.h"
 #include "wifi.h"
 #include "storage.h"
+#include "driver/gpio.h"
+static const int RX_BUF_SIZE = 1024;
 
+#define TXD_PIN (GPIO_NUM_22)
+#define RXD_PIN (GPIO_NUM_21)
+
+void init(void) {
+    const uart_config_t uart_config = {
+            .baud_rate = 115200,
+            .data_bits = UART_DATA_8_BITS,
+            .parity = UART_PARITY_DISABLE,
+            .stop_bits = UART_STOP_BITS_1,
+            .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+            .source_clk = UART_SCLK_APB,
+    };
+    // We won't use a buffer for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
 static const char *TAG = "fuckyou";
 
 static int server_socket = 0;                        // 服务器socket
@@ -76,7 +95,8 @@ void recv_data(void *pvParameters)
         g_rxtx_need_restart = false;
         if (len > 0){
             ESP_LOGI(TAG, "recvData: %s", databuff);//打印接收到的数组
-            send(connect_socket, databuff, strlen(databuff), 0);//接收数据回发
+            uart_write_bytes(UART_NUM_1, databuff, len);
+//            send(connect_socket, databuff, strlen(databuff), 0);//接收数据回发
             //sendto(connect_socket, databuff , sizeof(databuff), 0, (struct sockaddr *) &remote_addr,sizeof(remote_addr));
         }else{
           //  show_socket_error_reason("recv_data", connect_socket);//打印错误信息
@@ -134,6 +154,42 @@ static void get_ip_yes() {
     register_got_ip(NULL);
 }
 
+
+//int sendData(const char* logName, const char* data)
+//{
+//    const int len = strlen(data);
+//    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+//    ESP_LOGI(logName, "Wrote %d bytes", txBytes);
+//    return txBytes;
+//}
+
+//static void tx_task(void *arg)
+//{
+//    static const char *TX_TASK_TAG = "TX_TASK";
+//    esp_log_level_set(TX_TASK_TAG, ESP_LOG_INFO);
+//    while (1) {
+//        sendData(TX_TASK_TAG, "Hello world");
+//        vTaskDelay(2000 / portTICK_PERIOD_MS);
+//    }
+//}
+
+static void rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE+1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_RATE_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            send(connect_socket, data, rxBytes, 0);//接收数据回发
+           // ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            // ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+        }
+    }
+    free(data);
+}
+
 void app_main(void) {
     initNvs();
 
@@ -143,5 +199,9 @@ void app_main(void) {
     register_got_ip(wifi_callback);
     wifi_init_vaca();
 
+
+    init();
+    xTaskCreate(rx_task, "uart_rx_task", 1024*2, NULL, configMAX_PRIORITIES, NULL);
+    //xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-1, NULL);
 
 }
